15a371f74672b1ec31adcb63516dc84c2e824e73
Clemens Hesse-Edenfeld <9110170+chesseed@users.noreply.github.com>
2025-10-26 18:11:59 +0100

    add tests for methods

--- pkg/rest-aptly/client_test.go
+++ pkg/rest-aptly/client_test.go
@@ -5,35 +5,105 @@ import (
 	"testing"
 
 	"github.com/jarcoal/httpmock"
+	"github.com/maxatome/go-testdeep/td"
+	"github.com/maxatome/tdhttpmock"
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestGet(t *testing.T) {
-// 	httpmock.Activate(t)
-// 	client := NewClient("http://host.local")
+func TestGet(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
 
-// 	// Get the underlying HTTP Client and set it to Mock
-// 	httpmock.ActivateNonDefault(client.GetClient().GetClient())
+	type Body struct {
+		Msg string
+	}
+	response := Body{
+		Msg: "string",
+	}
 
-// 	type Body struct {
-// 		Msg string
-// 	}
-// 	response := Body{
-// 		Msg: "string",
-// 	}
+	httpmock.RegisterResponder(http.MethodGet, "http://host.local/get",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewJsonResponse(200, response)
+		})
 
-// 	httpmock.RegisterResponder(http.MethodGet, "http://host.local/get",
-// 		func(req *http.Request) (*http.Response, error) {
-// 			return httpmock.NewJsonResponse(200, response)
-// 		})
+	reqJSON := client.get("get")
+	resJSON, err := callAPIwithResult[Body](client, reqJSON)
+	assert.NoError(t, err)
+	assert.Equal(t, response, resJSON)
+}
 
-// 	resJSON, err := client.get("get").SetResult(&Body{}).Send()
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, &response, resJSON.Result().(*Body))
-// }
+func TestPost(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
+
+	type Body struct {
+		Msg string
+	}
+	response := Body{
+		Msg: "string",
+	}
+	request := Body{
+		Msg: "request body",
+	}
+
+	httpmock.RegisterMatcherResponder(http.MethodPost, "http://host.local/post",
+		tdhttpmock.JSONBody(td.JSON(`
+	{
+		"Msg": "request body"
+	}
+		`)),
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewJsonResponse(200, response)
+		})
+
+	reqJSON := client.post("post")
+	reqJSON.SetBody(&request)
+	resJSON, err := callAPIwithResult[Body](client, reqJSON)
+	assert.NoError(t, err)
+	assert.Equal(t, response, resJSON)
+}
+
+func TestPut(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
+
+	type Body struct {
+		Msg string
+	}
+	response := Body{
+		Msg: "string",
+	}
+
+	httpmock.RegisterResponder(http.MethodPut, "http://host.local/put",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewJsonResponse(200, response)
+		})
+
+	reqJSON := client.put("put")
+	resJSON, err := callAPIwithResult[Body](client, reqJSON)
+	assert.NoError(t, err)
+	assert.Equal(t, response, resJSON)
+}
+
+func TestDelete(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
+
+	type Body struct {
+		Msg string
+	}
+	response := Body{
+		Msg: "string",
+	}
+
+	httpmock.RegisterResponder(http.MethodDelete, "http://host.local/delete",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewJsonResponse(200, response)
+		})
+
+	reqJSON := client.delete("delete")
+	resJSON, err := callAPIwithResult[Body](client, reqJSON)
+	assert.NoError(t, err)
+	assert.Equal(t, response, resJSON)
+}
 
 func TestGetError(t *testing.T) {
-
 	client := clientForTest(t, "http://host.local")
 
 	httpmock.RegisterResponder(http.MethodGet, "http://host.local/json/content-type",
