70d879a9feb0c1d3968558b2d9978b54bba588fd
Clemens Hesse-Edenfeld <9110170+chesseed@users.noreply.github.com>
2025-10-26 18:14:56 +0100

    reenable simple files tests

--- pkg/rest-aptly/files_test.go
+++ pkg/rest-aptly/files_test.go
@@ -1,41 +1,61 @@
 package aptly
 
-// import (
-// 	"bytes"
-// 	"fmt"
-// 	"net/http"
-// 	"os"
-// 	"testing"
+import (
+	"net/http"
+	"testing"
 
-// 	"github.com/jarcoal/httpmock"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestFilesListDirs(t *testing.T) {
-// 	client := clientForTest(t, "http://host.local")
+func TestFilesListDirs(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
 
-// 	httpmock.RegisterResponder(http.MethodGet, "http://host.local/api/files",
-// 		func(req *http.Request) (*http.Response, error) {
-// 			return httpmock.NewJsonResponse(200, []string{"dir1", "dir2", "dir3"})
-// 		})
+	httpmock.RegisterResponder(http.MethodGet, "http://host.local/api/files",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewJsonResponse(200, []string{"dir1", "dir2", "dir3"})
+		})
 
-// 	list, err := client.FilesListDirs()
-// 	assert.NoError(t, err)
-// 	assert.ElementsMatch(t, list, []string{"dir1", "dir2", "dir3"})
-// }
+	list, err := client.FilesListDirs()
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, list, []string{"dir1", "dir2", "dir3"})
+}
 
-// func TestFilesListFiles(t *testing.T) {
-// 	client := clientForTest(t, "http://host.local")
+func TestFilesListFiles(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
 
-// 	httpmock.RegisterResponder(http.MethodGet, "http://host.local/api/files/dirTest",
-// 		func(req *http.Request) (*http.Response, error) {
-// 			return httpmock.NewJsonResponse(200, []string{"file1", "file2", "file3"})
-// 		})
+	httpmock.RegisterResponder(http.MethodGet, "http://host.local/api/files/dirTest",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewJsonResponse(200, []string{"file1", "file2", "file3"})
+		})
 
-// 	list, err := client.FilesListFiles("dirTest")
-// 	assert.NoError(t, err)
-// 	assert.ElementsMatch(t, list, []string{"file1", "file2", "file3"})
-// }
+	list, err := client.FilesListFiles("dirTest")
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, list, []string{"file1", "file2", "file3"})
+}
+
+func TestFilesDeleteDir(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
+
+	httpmock.RegisterResponder(http.MethodDelete, "http://host.local/api/files/dirTest",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewStringResponse(200, ""), nil
+		})
+
+	err := client.FilesDeleteDir("dirTest")
+	assert.NoError(t, err)
+}
+func TestFilesDeleteFile(t *testing.T) {
+	client := clientForTest(t, "http://host.local")
+
+	httpmock.RegisterResponder(http.MethodDelete, "http://host.local/api/files/dirTest/file",
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewStringResponse(200, ""), nil
+		})
+
+	err := client.FilesDeleteFile("dirTest", "file")
+	assert.NoError(t, err)
+}
 
 // func formFileEqual(req *http.Request, name string, data []byte) (bool, error) {
 // 	file, handler, err := req.FormFile(name)
@@ -84,27 +104,4 @@ package aptly
 // 	list, err := client.FilesUpload("dirTest", []string{f1.Name()})
 // 	assert.NoError(t, err)
 // 	assert.ElementsMatch(t, list, []string{"file0", "extra", "another"})
-// }
-
-// func TestFilesDeleteDir(t *testing.T) {
-// 	client := clientForTest(t, "http://host.local")
-
-// 	httpmock.RegisterResponder(http.MethodDelete, "http://host.local/api/files/dirTest",
-// 		func(req *http.Request) (*http.Response, error) {
-// 			return httpmock.NewStringResponse(200, ""), nil
-// 		})
-
-// 	err := client.FilesDeleteDir("dirTest")
-// 	assert.NoError(t, err)
-// }
-// func TestFilesDeleteFile(t *testing.T) {
-// 	client := clientForTest(t, "http://host.local")
-
-// 	httpmock.RegisterResponder(http.MethodDelete, "http://host.local/api/files/dirTest/file",
-// 		func(req *http.Request) (*http.Response, error) {
-// 			return httpmock.NewStringResponse(200, ""), nil
-// 		})
-
-// 	err := client.FilesDeleteFile("dirTest", "file")
-// 	assert.NoError(t, err)
 // }
